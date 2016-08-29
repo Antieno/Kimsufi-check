77c04094b6e1f7fd0d8849b7fcfe729da67ecde2
Antieno <Antieno@users.noreply.github.com>
2016-08-30 00:27:28 +0200

    Update kimsufi-check.sh

--- kimsufi-check.sh
+++ kimsufi-check.sh
@@ -2,15 +2,21 @@
 
 set -x
 
+cd /opt/prod/script/kimsuficheck/
+
 rm *.txt
 
 wget https://www.kimsufi.com/fr/serveurs.xml -O kimsufi.txt
 
-smsmax=2
+runmax=120
 
-counter=0
+run=0
 
-while [[ -z $result ]] && (( $counter < $smsmax )); #tant que la variable $result est vide et nombre sms envoyés sont inf ou égal au nombr de sms prédéfinis
+smsmax=5
+
+countersms=0
+
+while [[ -z $result ]] && (( $countersms < $smsmax )) && (( $run < $runmax )); #tant que la variable $result est vide et nombre sms envoyés sont inf ou égal au nombr de sms prédéfinis
 
 	do 
 
@@ -20,15 +26,17 @@ while [[ -z $result ]] && (( $counter < $smsmax )); #tant que la variable $resul
 		
 		if [[ -n $result ]] ; then #si la variable n'est pas vide
 		
-			curl -i -G "https://smsapi.free-mobile.fr/sendmsg?user="usernumber"&pass="password"&msg="yourtext""
+			curl -i -G "https://smsapi.free-mobile.fr/sendmsg?user=10113398&pass=AAm8ynqD0RF9m4&msg=Du changement sur la page Kimsufi, va vérifier Léon ! Fonce !!"
 			
-			counter=$(( $counter + 1 )) # on incrémente la variable avec +1
+			countersms=$(( $countersms + 1 )) # on incrémente la variable avec +1
 			
 			unset result # suppression de la variable
 			
 		fi
 		
 		rm kimsufi_2.txt
+		
+		run=$(( $run + 1 ))
 		
 		sleep 30
 		
